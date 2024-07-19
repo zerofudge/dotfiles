a4cf30b5eb4294a36389f2032092c9bc7d5f168c
Frank Geusch <f.geusch@syseleven.de>
2024-07-19 13:28:56 +0200

    chore(nvim): added simple session mgmt

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -17,6 +17,9 @@ M.general = {
     ["<C-l>"] = { "<cmd>TmuxNavigateRight<cr>", "Window right" },
     ["<C-j>"] = { "<cmd>TmuxNavigateDown<cr>", "Window down" },
     ["<C-k>"] = { "<cmd>TmuxNavigateUp<cr>", "Window up" },
+
+    ["<leader>ss"] = { "<cmd>mksession! ~/.local/share/nvim/session/global.vim<cr>", "save session" },
+    ["<leader>sl"] = { "<cmd>source ~/.local/share/nvim/session/global.vim<cr>", "load session" },
   },
   v = {
     [">"] = { ">gv", "indent"},
